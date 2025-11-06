01c82667820b4d14c82e58c9600b20a4f56ae7d1
LeeTaeYoung <xodud5080@naver.com>
2025-11-06 14:41:06 +0900

    controller, service connect

--- src/main/java/com/taeyoung/studyhub/studyhub_backend/controller/MemberController.java
+++ src/main/java/com/taeyoung/studyhub/studyhub_backend/controller/MemberController.java
@@ -1,45 +1,50 @@
 package com.taeyoung.studyhub.studyhub_backend.controller;
 
+import com.taeyoung.studyhub.studyhub_backend.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
 
     // 로그인 
     @PutMapping("/api/members/login")
     public String loginMember(){
-        
+        memberService.login();
         return "login";
     }
 
     // 회원가입
     @PutMapping("/api/members/signup")
     public String registerMember(){
-
+        memberService.registerMember();
         return "signup";
     }
 
     // 회원 정보 조회
     @GetMapping("/api/members/me")
     public String getMyInfo(){
-
+        memberService.getMyInfo();
         return "me";
     }
 
     // 회원 정보 수정
     @PutMapping("/api/members/me")
     public String updateMyInfo(){
-
+        memberService.updateMember();
         return "update";
     }
 
     // 회원 탈퇴
     @DeleteMapping("/api/members/me")
     public String deleteMyAccount(){
-
+        memberService.deleteMember();
         return "delete";
     }
 }
