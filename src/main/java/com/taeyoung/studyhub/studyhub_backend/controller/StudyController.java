52f752be91e1016ecc2cbd2f80b1e6963a79adaa
LeeTaeYoung <xodud5080@naver.com>
2025-11-18 20:21:10 +0900

    create study list html

--- src/main/java/com/taeyoung/studyhub/studyhub_backend/controller/StudyController.java
+++ src/main/java/com/taeyoung/studyhub/studyhub_backend/controller/StudyController.java
@@ -1,4 +1,15 @@
 package com.taeyoung.studyhub.studyhub_backend.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class StudyController {
+
+    @PostMapping("/api/studies")
+    public void findPassword() {
+
+    }
 }
