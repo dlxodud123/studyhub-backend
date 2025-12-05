4797b20ed59dda161e24b15a9e82050acf6d5dc1
LeeTaeYoung <xodud5080@naver.com>
2025-12-05 14:53:27 +0900

    modify modify.html

--- src/main/java/com/taeyoung/studyhub/studyhub_backend/controller/MemberController.java
+++ src/main/java/com/taeyoung/studyhub/studyhub_backend/controller/MemberController.java
@@ -79,12 +79,10 @@ public class MemberController {
             return ResponseEntity.ok("회원수정 성공!");
 
         } catch (IllegalArgumentException e) {
-            // 회원이 존재하지 않거나 검증 실패
             return ResponseEntity
                     .badRequest()
                     .body(e.getMessage());
         } catch (RuntimeException e) {
-            // 기타 서버 오류
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("서버 오류가 발생했습니다.");
