887c77433450da839dfa13b8758f87f4da849f93
LeeTaeYoung <xodud5080@naver.com>
2025-12-06 13:34:50 +0900

    modify createRequestDto

--- src/main/java/com/taeyoung/studyhub/studyhub_backend/dto/study/request/StudyCreateRequestDto.java
+++ src/main/java/com/taeyoung/studyhub/studyhub_backend/dto/study/request/StudyCreateRequestDto.java
@@ -1,13 +1,18 @@
 package com.taeyoung.studyhub.studyhub_backend.dto.study.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class StudyCreateRequestDto {
+
+    @NotBlank(message = "제목을 입력해주세요.")
     private String title;
+    @NotBlank(message = "내용을 입력해주세요.")
     private String content;
+    @NotBlank(message = "카테고리를 입력해주세요.")
     private Long categoryId;
     private List<String> tagNames;
 
