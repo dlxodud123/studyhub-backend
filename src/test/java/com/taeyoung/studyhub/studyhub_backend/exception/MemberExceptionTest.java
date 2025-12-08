b2019a25f57fbcb74ce4129bacef8210147e194c
LeeTaeYoung <xodud5080@naver.com>
2025-12-08 13:23:55 +0900

    exception test(signup)

--- src/test/java/com/taeyoung/studyhub/studyhub_backend/exception/MemberExceptionTest.java
+++ src/test/java/com/taeyoung/studyhub/studyhub_backend/exception/MemberExceptionTest.java
@@ -2,14 +2,19 @@ package com.taeyoung.studyhub.studyhub_backend.exception;
 
 import com.taeyoung.studyhub.studyhub_backend.domain.member.ProviderType;
 import com.taeyoung.studyhub.studyhub_backend.dto.member.request.SignupRequestDto;
+import com.taeyoung.studyhub.studyhub_backend.global.exception.DuplicateEmailException;
+import com.taeyoung.studyhub.studyhub_backend.global.exception.DuplicateUsernameException;
 import com.taeyoung.studyhub.studyhub_backend.repository.member.MemberRepository;
 import com.taeyoung.studyhub.studyhub_backend.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -28,5 +33,23 @@ public class MemberExceptionTest {
         memberService.registerMember(signupRequestDto2);
     }
 
-    
+    @Test
+    public void signupException() {
+        // given
+        SignupRequestDto signupRequestDto1 = new SignupRequestDto("user1", "password1", "email", ProviderType.LOCAL);
+        SignupRequestDto signupRequestDto2 = new SignupRequestDto("user", "password1", "email1", ProviderType.LOCAL);
+
+        // when, then
+        assertThatThrownBy(() ->
+            memberService.registerMember(signupRequestDto1)
+        )
+            .isInstanceOf(DuplicateUsernameException.class)
+            .hasMessage("이미 사용중인 username입니다.");
+
+        assertThatThrownBy(() ->
+                memberService.registerMember(signupRequestDto2)
+        )
+                .isInstanceOf(DuplicateEmailException.class)
+                .hasMessage("이미 사용중인 email입니다.");
+    }
 }
