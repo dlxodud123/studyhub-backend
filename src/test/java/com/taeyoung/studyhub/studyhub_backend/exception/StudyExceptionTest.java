4621e60b891ea457386cee03cde75c14303c0a90
LeeTaeYoung <xodud5080@naver.com>
2025-12-08 13:48:41 +0900

    exception test(before)

---
+++ src/test/java/com/taeyoung/studyhub/studyhub_backend/exception/StudyExceptionTest.java
@@ -0,0 +1,59 @@
+package com.taeyoung.studyhub.studyhub_backend.exception;
+
+import com.taeyoung.studyhub.studyhub_backend.domain.member.Member;
+import com.taeyoung.studyhub.studyhub_backend.domain.member.ProviderType;
+import com.taeyoung.studyhub.studyhub_backend.domain.study.Category;
+import com.taeyoung.studyhub.studyhub_backend.domain.study.Comment;
+import com.taeyoung.studyhub.studyhub_backend.domain.study.Study;
+import com.taeyoung.studyhub.studyhub_backend.dto.member.request.SignupRequestDto;
+import com.taeyoung.studyhub.studyhub_backend.dto.study.request.StudyCreateRequestDto;
+import com.taeyoung.studyhub.studyhub_backend.repository.study.CategoryRepository;
+import com.taeyoung.studyhub.studyhub_backend.repository.study.CommentRepository;
+import com.taeyoung.studyhub.studyhub_backend.repository.study.StudyRepository;
+import com.taeyoung.studyhub.studyhub_backend.service.MemberService;
+import com.taeyoung.studyhub.studyhub_backend.service.StudyService;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class StudyExceptionTest {
+
+    @Autowired private StudyRepository studyRepository;
+    @Autowired private CategoryRepository categoryRepository;
+    @Autowired private CommentRepository commentRepository;
+    @Autowired private StudyService studyService;
+    @Autowired private MemberService memberService;
+
+    private Member member1;
+    private Member member2;
+    private Study study1;
+    private Study study2;
+    private Comment comment1;
+    private Comment comment2;
+
+    @BeforeEach
+    public void before() {
+        SignupRequestDto signupRequestDto1 = new SignupRequestDto("user1", "password1", "email1", ProviderType.LOCAL);
+        SignupRequestDto signupRequestDto2 = new SignupRequestDto("user2", "password2", "email2", ProviderType.GOOGLE);
+        member1 = memberService.registerMember(signupRequestDto1);
+        member2 = memberService.registerMember(signupRequestDto2);
+        Category category1 = categoryRepository.save(new Category("testCategory1"));
+        Category category2 = categoryRepository.save(new Category("testCategory2"));
+        StudyCreateRequestDto dto1 = new StudyCreateRequestDto("testTitle1", "testContent1", category1.getId(), List.of("testTag1", "testTag2"));
+        StudyCreateRequestDto dto2 = new StudyCreateRequestDto("testTitle2", "testContent2", category2.getId(), List.of("testTag3", "testTag4"));
+        study1 = studyService.createStudy(dto1, member1.getId());
+        study2 = studyService.createStudy(dto2, member2.getId());
+        comment1 = studyService.createComment("testComment1", member1.getId(), study1.getId());
+        comment2 = studyService.createComment("testComment2", member1.getId(), study1.getId());
+    }
+
+    
+}
