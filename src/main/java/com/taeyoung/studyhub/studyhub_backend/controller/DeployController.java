08ad2268ac54d88c95e994cdfe4c36a234de01e3
LeeTaeYoung <xodud5080@naver.com>
2025-12-17 11:34:57 +0900

    deploy.yml 29

--- src/main/java/com/taeyoung/studyhub/studyhub_backend/controller/DeployController.java
+++ src/main/java/com/taeyoung/studyhub/studyhub_backend/controller/DeployController.java
@@ -63,5 +63,9 @@ public class DeployController {
     public String deploy11() {
         return "축하합니다!진짜로!asdf11111111111111111";
     }
-     
+
+    @GetMapping("/deploy/12")
+    public String deploy12() {
+        return "축하합니다!진짜로!12";
+    }
 }
